fd968d38a7fe3110cd528e719cc6c926ec7ed2ce
fgilde <fgilde@gmail.com>
2024-09-26 10:08:08 +0200

    -

--- Aimmy2/UILibrary/MultiKeyChanger.xaml.cs
+++ Aimmy2/UILibrary/MultiKeyChanger.xaml.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using Aimmy2;
 using Aimmy2.InputLogic;
